7bfdb099185262960b4d9640f4be51e27e3f2ae8
DMTyler <1171899045@qq.com>
2025-01-31 00:54:35 +0800

    Update Project Structure

---
+++ Scripts/Simple/Pipeline/DissipationRendererFeature.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+using UnityEngine.Rendering;
+using UnityEngine.Rendering.Universal;
+
+namespace DGraphics.Dissipation.Simple
+{
+    public class DissipationRendererFeature : ScriptableRendererFeature
+    {
+        private DissipationRendererPass _pass;
+
+        private class DissipationRendererPass : ScriptableRenderPass
+        {
+            private ProfilingSampler _profilingSampler = new("DissipationRendererPass");
+
+            public override void Execute(ScriptableRenderContext context, ref RenderingData renderingData)
+            {
+                var cmd = CommandBufferPool.Get("DissipationRendererPass");
+                try
+                {
+                    using (new ProfilingScope(cmd, _profilingSampler))
+                    {
+                        MeshDissipationController.InjectCommand(cmd, context, ref renderingData);
+                    }
+                }
+                finally
+                {
+                    CommandBufferPool.Release(cmd);
+                }
+                
+            }
+        }
+
+        public override void Create()
+        {
+            _pass = new DissipationRendererPass();
+        }
+
+        public override void AddRenderPasses(ScriptableRenderer renderer, ref RenderingData renderingData)
+        {
+            if (_pass == null)
+            {
+                Debug.LogError("DissipationRendererPass is null");
+                return;
+            }
+
+            renderer.EnqueuePass(_pass);
+            _pass.renderPassEvent = RenderPassEvent.BeforeRendering;
+        }
+    }
+}
